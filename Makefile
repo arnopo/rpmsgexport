c17a1e930bd2d2617b80a1c497c4f51314234721
Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
2021-02-03 17:56:58 +0100

    Add ping test to test /dev/rpmsgX
    The ping test can test up to 2 /dev/rpmsgX device devices in parallel.

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 EPT  = rpmsgexport
 EPTD = rpmsgdestroyept
+PING = ping
 
 CFLAGS := -Wall -g -O2
 LDFLAGS := 
@@ -7,21 +8,30 @@ prefix := /usr/local
 
 SRCS := rpmsgexport.c
 SRCS += rpmsgdestroyept.c
+SRCS += ping.c
 OBJS := $(SRCS:.c=.o)
 
-all: $(EPT) $(EPTD)
+all: $(EPT) $(EPTD) $(PING)
 .PHONY : all
+
+$(EPT): rpmsgexport.o
+	$(CC) $(LDFLAGS) -o $@ $^
 
 $(EPTD): rpmsgdestroyept.o
 	$(CC) $(LDFLAGS) -o $@ $^
 
-install: $(EPT) $(EPTD)
+$(PING): ping.o
+	$(CC) $(LDFLAGS) -o $@ $^
+
+install: $(EPT) $(EPTD) $(PING)
 	install -D -m 755 $(EPT) $(DESTDIR)$(prefix)/bin/$(EPT)
 	install -D -m 755 $(EPTD) $(DESTDIR)$(prefix)/bin/$(EPTD)
+	install -D -m 755 $(PING) $(DESTDIR)$(prefix)/bin/$(PING)
 
 clean:
-	rm -f $(EPT) $(EPTD) $(OBJS)
+	rm -f $(EPT) $(EPTD) $(PING) $(OBJS)
 
 distclean:
 	rm -f $(DESTDIR)$(prefix)/bin/$(EPT)
 	rm -f $(DESTDIR)$(prefix)/bin/$(EPTD)
+	rm -f $(DESTDIR)$(prefix)/bin/$(PING)
---
+++ ping.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2016, Linaro Ltd.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <sys/ioctl.h>
+
+#include <err.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+static void usage(void)
+{
+	extern char *__progname;
+
+	fprintf(stderr, "%s <dev> <dev2>\n", __progname);
+	exit(1);
+}
+
+int main(int argc, char **argv)
+{
+	char buffer[12];
+	int ret;
+	int fd, fd2 = 0;
+
+	if (argc != 2 && argc != 3)
+		usage();
+
+	fd = open(argv[1], O_RDWR);
+	if (fd < 0)
+		err(1, "failed to open %s\n", argv[1]);
+	if (argc == 3) {
+		fd2 = open(argv[2], O_RDWR);
+		if (fd2 < 0)
+			err(1, "failed to open %s\n", argv[2]);
+	}
+	ret = write(fd, "ping dev1!",11);
+	if (ret < 0) {
+		fprintf(stderr, "failed to write endpoint  %s", argv[1]);
+		exit(1);
+	}
+	ret = read(fd, buffer,11);
+	if (ret < 0) {
+		fprintf(stderr, "failed to read endpoint %s", argv[1]);
+		exit(1);
+	}
+	fprintf(stderr, "result for %s: %s\n", argv[1], buffer);
+
+	if (fd2) {
+		ret = write(fd2, "ping dev2!",11);
+		if (ret < 0) {
+			fprintf(stderr, "failed to write endpoint  %s", argv[2]);
+			exit(1);
+		}
+		ret = read(fd2, buffer,11);
+		if (ret < 0) {
+			fprintf(stderr, "failed to read endpoint  %s", argv[2]);
+			exit(1);
+		}
+		fprintf(stderr, "result for %s: %s\n", argv[2], buffer);
+	}
+	
+	close(fd);
+	if (fd2)
+		close(fd2);
+	return 0;
+}
